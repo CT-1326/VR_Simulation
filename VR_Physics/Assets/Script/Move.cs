cb2efbe55dd3dccb4ebe85c6431a027afd095691
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-04-12 04:06:46 +0900

    First

---
+++ VR_Physics/Assets/Script/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+    public static int check;
+
+    public void Mgo()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void Lgo()
+    {
+        SceneManager.LoadScene("Left");
+    }
+    public void Rgo()
+    {
+        SceneManager.LoadScene("Right");
+    }
+
+    public void V1()
+    {
+        check = 1;
+        SceneManager.LoadScene("Movie");
+    }
+    public void V2()
+    {
+        check = 2;
+        SceneManager.LoadScene("Movie");
+    }
+    public void V3()
+    {
+        check = 3;
+        SceneManager.LoadScene("Movie");
+    }
+}
---
+++ VR_Physics/Assets/Script/Video.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+    public GameObject go;
+    public GameObject go2;
+    public GameObject go3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Move.check==1)
+        {
+            go.SetActive(true);
+        }
+        else if (Move.check == 2)
+        {
+            go2.SetActive(true);        
+        }
+        else if (Move.check == 3)
+        {
+            go3.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
