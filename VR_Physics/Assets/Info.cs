c20e6aff4932a09d176f0cede4f0ad758864b3b1
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-05-01 04:05:06 +0900

    ADD

---
+++ VR_Physics/Assets/Info.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    public GameObject go;
+    public GameObject go2;
+    public GameObject go3;
+
+    public void show()
+    {
+        go2.SetActive(false);
+        go3.SetActive(false);
+        go.SetActive(true);
+    }
+
+    public void exit()
+    {
+        go.SetActive(false);
+        go2.SetActive(true);
+        go3.SetActive(true);
+    }
+}
