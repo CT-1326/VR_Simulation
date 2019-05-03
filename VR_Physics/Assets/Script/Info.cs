e1a0bb28e230b7daff008c3008686f0a3d38417e
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-05-04 00:41:15 +0900

    Add & Fix

---
+++ VR_Physics/Assets/Script/Info.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour
+{
+    Text txt;
+
+    public GameObject go;
+
+    public void TextView()
+     {
+        go.SetActive(true);
+
+         txt = GameObject.Find("Text").GetComponent<Text>();
+
+         if (Move.check == 1)
+         {
+            txt.text = "안녕하세요\n안녕들하십니까 안녕하십니까";
+         }
+         else if (Move.check == 2)
+         {
+            txt.text = "안녕하세요2\n\n안녕들하십니까2  안녕하십니까2";
+        }
+         else if (Move.check == 3)
+         {
+            txt.text = "안녕하세요3\n\n\n안녕들하십니까3   안녕하십니까3";
+        }
+     }
+
+    public void Exit()
+    {
+        go.SetActive(false);
+    }
+}
--- VR_Physics/Assets/Script/Video.cs
+++ VR_Physics/Assets/Script/Video.cs
@@ -1,33 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Video : MonoBehaviour
 {
     public GameObject go;
     public GameObject go2;
     public GameObject go3;
-    // Start is called before the first frame update
+
     void Start()
     {
-        if(Move.check==1)
-        {
+        if (Move.check == 1)      
             go.SetActive(true);
-        }
+        
         else if (Move.check == 2)
-        {
-            go2.SetActive(true);        
-        }
-        else if (Move.check == 3)
-        {
-            go3.SetActive(true);
-        }
+            go2.SetActive(true);
 
+        else if (Move.check == 3)
+            go3.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
