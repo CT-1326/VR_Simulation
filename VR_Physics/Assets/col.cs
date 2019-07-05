f4f8c4f873e4cf5b0edcf5acd45779939dc37926
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-07-05 21:12:07 +0900

    ADD

---
+++ VR_Physics/Assets/col.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class col : MonoBehaviour
+{
+
+    public GameObject txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="player")
+        {
+            StartCoroutine(work());
+        }
+    }
+
+    IEnumerator work()
+    {
+        txt.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        txt.SetActive(false);
+    }
+}
