4b97336923772522f0096990494ec02b43f9cfd0
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-07-18 22:01:36 +0900

    ADD

---
+++ VR_Physics/Assets/show_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class show_move : MonoBehaviour
+{
+    public GameObject go;
+    TextMeshPro txt;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<TextMeshPro>().text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //txt.text = "X : "+go.transform.localPosition.x +"Y : "+go.transform.localPosition.y;
+        txt.text="hi";
+    }
+}
