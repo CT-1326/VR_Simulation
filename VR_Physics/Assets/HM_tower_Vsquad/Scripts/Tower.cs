1ff6ffedae3e47f2ad18bf2b93ea61bbd6eff2b7
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-07-20 17:58:39 +0900

    FIX

--- VR_Physics/Assets/HM_tower_Vsquad/Scripts/Tower.cs
+++ VR_Physics/Assets/HM_tower_Vsquad/Scripts/Tower.cs
@@ -3,17 +3,14 @@ using System.Collections;
 
 public class Tower : MonoBehaviour
 {
+    public Transform shootElement; // 대포알 발사 시작점
+    public GameObject bullet; // 대포알 모델
 
-    public Transform shootElement;
-    public GameObject bullet;
+    float speed = .2f; // 대포알 스피드 값
+    double fireTimer = 0.0; // 대포알 하나씩 끊어서 발사하기 위한 발사 시간
+    double fireTimerLength = .25; // 발사 시간 최대 값
 
-    Vector3 offset = new Vector3(.15f, 0, -.15f);
-
-    float speed = .2f;
-    double fireTimer = 0.0;
-    double fireTimerLength = .25;
-
-    bool isShoot = false;
+    bool isShoot = false; // 대포 발사 여부
 
     void Start()
     {
@@ -22,9 +19,9 @@ public class Tower : MonoBehaviour
 
     void Update()
     {
-        if (isShoot)
+        if (isShoot) // 대포가 발사 되었을 때
         {
-            fireTimer += Time.deltaTime;
+            fireTimer += Time.deltaTime; // 1초씩 카운트해서 최대 값 만큼 도달시킨다
             if (fireTimer >= fireTimerLength)
             {
                 fireTimer = 0.0;
@@ -32,19 +29,17 @@ public class Tower : MonoBehaviour
             }
         }
 
-        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad) || Input.GetKey(KeyCode.R))
+        if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad) || Input.GetKey(KeyCode.R)) // 특정 키가 눌렸을 때
         {
-            // if is not already firing, start firing
-            if (!isShoot)
+            if (!isShoot) // 대포알 발사 전이면
             {
-                // Get force value from slider
-                float force = GameObject.Find("Slider").GetComponent<UnityEngine.UI.Slider>().value;
+                float force = GameObject.Find("Slider").GetComponent<UnityEngine.UI.Slider>().value; // ui의 슬라이더 값을 실수로 받는다
                 Debug.Log("Fire! " + force);
-                isShoot = true;
+                isShoot = true; // 대포알이 발사 되었음을 알림
                 // Instantiate new satellite
-                GameObject bulletClone = (GameObject)Instantiate(bullet, shootElement.position,shootElement.rotation);
+                GameObject bulletClone = (GameObject)Instantiate(bullet, shootElement.position,shootElement.rotation); // 복제본을 만들어 하나하나 독립적인 객체 대포알을 만든다
                 // Set velocty on satellite
-                bulletClone.GetComponent<Rigidbody>().velocity = transform.forward * (speed * force);
+                bulletClone.GetComponent<Rigidbody>().velocity = transform.forward * (speed * force); // 대포알이 앞으로 나가는데 기존 스피드 값과 슬라이더 값의 곱한 것 만큼 나갈 수 있게 해준다
             }
         }
 
--- VR_Physics/Assets/HM_tower_Vsquad/Scripts/bulletTower.cs
+++ VR_Physics/Assets/HM_tower_Vsquad/Scripts/bulletTower.cs
@@ -4,20 +4,15 @@ using System.Collections;
 public class bulletTower : MonoBehaviour
 {
     Vector3 gravityPoint = new Vector3(0, 0, 0);
-    float rotationsPerMinute = 10;
-    ///Quaternion targetRotation;
-    // Use this for initialization
+ 
     void Start()
     {
-        //targetRotation = Quaternion.Euler(0, 360, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.GetComponent<Rigidbody>().AddForce((gravityPoint - transform.position).normalized);
-        //transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * 6f );
-        //transform.Rotate(0, 0, 6.0f * rotationsPerMinute * Time.deltaTime);
     }
 
 }
